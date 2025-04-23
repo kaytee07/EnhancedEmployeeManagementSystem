1fa094b86365fded2a36a5abeecb52a4e523853b
kaytee07 <kofi.taylor07@gmail.com>
2025-04-23 04:49:37 +0000

    Test: add test to check if search employee by department returns employees of that department

--- src/test/java/com/example/employeesystem/Model/EmployeeDatabaseTest.java
+++ src/test/java/com/example/employeesystem/Model/EmployeeDatabaseTest.java
@@ -28,6 +28,37 @@ class EmployeeDatabaseTest {
     }
 
     @Test
-    void search() {
+    void search_employeeByDepartmentFound() throws Exception {
+        Employee<UUID> emp1 = new Employee<>("Alice", "HR", 3500.0, 4.2, 5);
+        Employee<UUID> emp2 = new Employee<>( "Bob", "Engineering", 5000.0, 3.8, 7);
+        Employee<UUID> emp3 = new Employee<>( "Charlie", "Sales", 4200.0, 4.5, 3);
+        Employee<UUID> emp4 = new Employee<>( "Diana", "Marketing", 3900.0, 4.1, 4);
+        Employee<UUID> emp5 = new Employee<>( "Ethan", "Engineering", 6000.0, 4.8, 10);
+        EmployeeDatabase<UUID> dataBase = new EmployeeDatabase<>();
+        dataBase.addEmployee(emp1);
+        dataBase.addEmployee(emp2);
+        dataBase.addEmployee(emp3);
+        dataBase.addEmployee(emp4);
+        dataBase.addEmployee(emp5);
+
+        Assertions.assertFalse(dataBase.search("Engineering").isEmpty());
+    }
+
+    @Test
+    void search_employeeByDepartmentNotFound() throws Exception {
+        Employee<UUID> emp1 = new Employee<>("Alice", "HR", 3500.0, 4.2, 5);
+        Employee<UUID> emp2 = new Employee<>( "Bob", "Engineering", 5000.0, 3.8, 7);
+        Employee<UUID> emp3 = new Employee<>( "Charlie", "Sales", 4200.0, 4.5, 3);
+        Employee<UUID> emp4 = new Employee<>( "Diana", "Marketing", 3900.0, 4.1, 4);
+        Employee<UUID> emp5 = new Employee<>( "Ethan", "Engineering", 6000.0, 4.8, 10);
+
+        EmployeeDatabase<UUID> dataBase = new EmployeeDatabase<>();
+        dataBase.addEmployee(emp1);
+        dataBase.addEmployee(emp2);
+        dataBase.addEmployee(emp3);
+        dataBase.addEmployee(emp4);
+        dataBase.addEmployee(emp5);
+
+        Assertions.assertTrue(dataBase.search("Devops").isEmpty());
     }
 }
